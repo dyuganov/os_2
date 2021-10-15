c0be0301be32b40c952c53f24ad70396e138f5e2
dyuganov-n <dyuganov.n@gmail.com>
2021-10-16 02:07:11 +0700

    lab6 fix

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -5,25 +5,28 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define STRING_NUM (20)
 #define MAX_STRING_LENGTH (256)
-#define PROPORTIONAL_RATION (100)
+#define PROPORTIONAL_RATION (1000)
 
 #define PTHREAD_SUCCESS (0)
 #define BUFFER_SIZE (256)
+#define LINES_NUM_CNT_ERR (-1)
 
-pthread_t thread_id[STRING_NUM];
+#define SUCCESS (0)
+#define FAIL (1)
 
-void* threadJob(void* arg) {
+
+
+void* threadSortJob(void* arg) {
     char* string = (char*) arg;
     int length = strnlen(string, MAX_STRING_LENGTH);
 
-    usleep(length * PROPORTIONAL_RATION * PROPORTIONAL_RATION);
+    usleep(length * PROPORTIONAL_RATION);
 
-    printf("Length = %d -> ",length);
+    printf("Length = %d -> ", length);
     printf("%s", string);
 
-    pthread_exit(0);
+    pthread_exit(SUCCESS);
 }
 
 bool isThreadsError(int errorCode){
@@ -36,34 +39,70 @@ bool isThreadsError(int errorCode){
     return false;
 }
 
+int getLinesNum(FILE* file){
+    if(NULL == file) {
+        return LINES_NUM_CNT_ERR;
+    }
+    int result = 0;
+    char currChar = 0;
+    while(!feof(file)){
+        currChar = fgetc(file);
+        if('\n' == currChar){
+            result += 1;
+        }
+    }    
+    fseek(file, 0, SEEK_SET);
+    return result;
+}
+
+bool isGetLinesNumError(int getLinesNumResult){
+    if(LINES_NUM_CNT_ERR == getLinesNumResult){
+        fprintf(stderr, "Can't count strings num\n");
+        return true;
+    }
+    return false;
+}
+
 int main (int argc, char** argv) {
     if(2 > argc) fprintf(stderr, "Wrong args num\n");
 
     FILE* file = fopen(argv[1], "r");
     if(NULL == file) {
         fprintf(stderr, "Can't open the file\n");
-        exit(1);
+        pthread_exit(FAIL);
+        exit(FAIL);
     }
 
-    char strings[STRING_NUM][MAX_STRING_LENGTH];
-    for(int i = 0; i < STRING_NUM; i++) {
+    const int STRINGS_NUM = getLinesNum(file);
+    if(isGetLinesNumError(STRINGS_NUM)){
+        fclose(file);
+        pthread_exit(FAIL);
+        exit(FAIL);
+    }
+
+    char strings[STRINGS_NUM][MAX_STRING_LENGTH];
+    for(int i = 0; i < STRINGS_NUM; i++) {
         fgets(strings[i], MAX_STRING_LENGTH, file);
     }
     fclose(file);
 
-    for(int i = 0; i < STRING_NUM; i++) {
-        int createResult = pthread_create(&thread_id[i], NULL, threadJob, strings[i]);
+    pthread_t thread_id[STRINGS_NUM];
+    for(int i = 0; i < STRINGS_NUM; i++) {
+        int createResult = pthread_create(&thread_id[i], NULL, threadSortJob, strings[i]);
         if(isThreadsError(createResult)){
-            exit(1);
+            pthread_exit(FAIL);
+            exit(FAIL);
         }
     }
 
-    for(int i = 0; i < STRING_NUM; i++) {
+    for(int i = 0; i < STRINGS_NUM; i++) {
         int joinResult = pthread_join(thread_id[i], NULL);
         if(isThreadsError(joinResult)) {
-            exit(1);
+            pthread_exit(FAIL);
+            exit(FAIL);
         }
     }
 
+    pthread_exit(SUCCESS);
     return 0;
 }
