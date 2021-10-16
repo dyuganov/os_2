4bf1f3d53d0130f07675bccfb2785a370805263d
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-10-16 07:18:10 +0700

    Update main.c

--- task8/main.c
+++ task8/main.c
@@ -4,16 +4,19 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define NUM_STEPS 200000000
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define NUM_STEPS 10000000
 #define SUCCESS 0
 #define ERROR -1
 #define BUFFER_SIZE 256
 
-int threadsNumber;
+long int threadsNumber;
+long int iterForOneThread;
 
 struct threadInfo {
-    pthread_t threadId;
-    int thread;
+    long int thread;
     double result;
 };
 
@@ -31,21 +34,21 @@ int checkInput(int argc, char** argv) {
 }
 
 void* calculate(void *threads) {
-    double local_pi = 0.0;
-    int i = ((struct threadInfo *)threads)->thread;
-    for (; i < NUM_STEPS ; i += threadsNumber) {
-        local_pi += 1.0 / (i * 4.0 + 1.0);
-        local_pi -= 1.0 / (i * 4.0 + 3.0);
+    struct threadInfo localStruct = *((struct threadInfo*) threads);
+    for (long int i = localStruct.thread; i < (iterForOneThread + localStruct.thread) ; ++i) {
+        localStruct.result += 1.0 / (i * 4.0 + 1.0);
+        localStruct.result -= 1.0 / (i * 4.0 + 3.0);
     }
-    ((struct threadInfo *)threads)->result = local_pi;
-    return threads;
+    ((struct threadInfo *)threads)->result = localStruct.result;
+    pthread_exit(threads);
 }
 
-bool isThreadError(int errorCode, char *argv[], struct threadInfo *threads) {
+bool isThreadError(int errorCode, char *argv[], struct threadInfo *threads, pthread_t *threadId) {
     if (errorCode != SUCCESS) {
         char message[BUFFER_SIZE];
         strerror_r(errorCode, message, sizeof message);
         free(threads);
+        free(threadId);
         fprintf(stderr, "%s: Error: %s\n", argv[0], message);
         return true;
     }
@@ -56,31 +59,37 @@ int main(int argc, char** argv) {
     if (checkInput(argc, argv) == ERROR) {
         pthread_exit((void *) ERROR);
     }
+    double pi = 0.0;
+    pthread_t *threadId = (pthread_t*) malloc(threadsNumber * sizeof(pthread_t));
     struct threadInfo *threads = (struct threadInfo*)malloc(threadsNumber * sizeof(struct threadInfo));
-    if (threads == NULL) {
+    if (threads == NULL || threadId == NULL) {
         fprintf(stderr, "error with malloc()\n");
         return EXIT_FAILURE;
     }
+    iterForOneThread = NUM_STEPS / threadsNumber;
 
     int errorCode;
-    for (int i = 0; i < threadsNumber; ++i) {
-        errorCode = pthread_create(&(threads[i].threadId), NULL, calculate, (void *)(threads + i));
-        if (isThreadError(errorCode, argv, threads)) {
+    for (long int i = 0; i < threadsNumber; ++i) {
+        threads[i].thread = i * iterForOneThread;
+        threads[i].result = 0;
+        errorCode = pthread_create(&(threadId[i]), NULL, calculate, &(threads[i]));
+        if (isThreadError(errorCode, argv, threads, threadId)) {
             pthread_exit((void *) ERROR);
         }
-        threads[i].thread = i;
     }
 
-    double pi = 0.0;
-    for (int i = 0; i < threadsNumber; ++i) {
-        errorCode = pthread_join(threads[i].threadId, NULL);
-        if (isThreadError(errorCode, argv, threads)) {
+    for (long int i = 0; i < threadsNumber; ++i) {
+        struct threadInfo *localResult;
+        errorCode = pthread_join(threadId[i], (void **) &localResult);
+        if (isThreadError(errorCode, argv, threads, threadId)) {
             pthread_exit((void *) ERROR);
         }
-        pi += threads[i].result;
+        pi += localResult[i].result;
     }
     pi = pi * 4.0;
-    printf("Pi = %.13f\n", pi);
+    printf("Pi = %.15f\n", pi);
+    printf("Pi = %.15f\n", M_PI);
     free(threads);
+    free(threadId);
     pthread_exit(SUCCESS);
 }
