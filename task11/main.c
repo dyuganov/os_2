1b7b6a2bb70996449fe2921d4c8294d025416f9c
mmalakhova <m.malakhova@g.nsu.ru>
2021-10-23 00:00:56 +0700

    adding correct version of lab11

--- task11/main.c
+++ task11/main.c
@@ -1,43 +1,91 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
-#define PUTENV_SUCCESS 0
+#define COMMON 0
+#define PARENT 1
+#define CHILD 2
 
-extern char** environ;
+#define SUCCESS 0
 
-int execvpe(char* file, char* argv[], char* envp[]) {
-    if(file == NULL) {
-        perror("NULL pointer to file");
-        return EXIT_FAILURE;
+// 0 == "nobody's (common) mutex"
+// 1 == "parent's mutex"
+// 2 == "child's mutex"
+
+pthread_t thread;
+
+pthread_mutex_t mutexes[3];
+
+void destroyMutexes() {
+    for (int i = 0; i < 3; i++) {
+        pthread_mutex_destroy(&mutexes[i]);
     }
-    char** savedEnviron = environ;
-    environ = envp;
-
-    execvp(file, argv);
-	environ = savedEnviron;
-	perror("exec error");
-	return EXIT_FAILURE;
 }
 
-int main(int argc, char* argv[], char* envp[]) {
+void lockMutex(int mutexID) {
+    pthread_mutex_lock(&mutexes[mutexID]);
+}
 
-    if(argc < 2) {
-        fprintf(stderr, "Usage: %s [executable] <args>\n", argv[0]);
-        return EXIT_FAILURE;
+void unlockMutex(int mutexID) {
+    pthread_mutex_unlock(&mutexes[mutexID]);
+}
+
+
+void joinChildThread() {
+    int status = pthread_join(thread, NULL);
+    if (status != SUCCESS) {
+        printf("Error couldn't join thread = %d\n", status);
+        pthread_exit(NULL);
+    }
+}
+
+void* childPrintStrings() {
+    for (int i = 1; i <= 10; i++) {
+        lockMutex(PARENT);
+        printf("CHILD THREAD: this is string №%d\n", i);
+        unlockMutex(CHILD);
+        lockMutex(COMMON);
+        unlockMutex(PARENT);
+        lockMutex(CHILD);
+        unlockMutex(COMMON);
+    }
+    unlockMutex(CHILD);
+}
+
+void startChildThread() {
+    lockMutex(CHILD);
+    int status = pthread_create(&thread, NULL, childPrintStrings, NULL);
+    if (status != SUCCESS) {
+        printf("Error couldn't create thread = %d\n", status);
+        pthread_exit(NULL);
+    }
+}
+
+void parentPrintStrings() {
+    for (int i = 1; i <= 10; i++) {
+        printf("PARENT THREAD: this is string №%d\n", i);
+        lockMutex(COMMON);
+        unlockMutex(PARENT);
+        lockMutex(CHILD);
+        unlockMutex(COMMON);
+        lockMutex(PARENT);
+        unlockMutex(CHILD);
+    }
+    unlockMutex(PARENT);
+}
+
+int main() {
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+    for (int i = 0; i < 3; i++) {
+        pthread_mutex_init(&mutexes[i], &attr);
     }
 
-    if(envp == NULL) {
-        perror("Environment is not set");
-    }
-
-    if(putenv("TZ=PST8PDT") != PUTENV_SUCCESS){
-        perror("putenv error");
-	return EXIT_FAILURE;
-    }
-
-    execvpe(argv[1], &argv[1], envp);
-    perror("exec error");
-
-    return EXIT_FAILURE;
+    lockMutex(PARENT);
+    startChildThread();
+    sleep(1);
+    parentPrintStrings();
+    joinChildThread();
+    destroyMutexes();
+    return 0;
 }
