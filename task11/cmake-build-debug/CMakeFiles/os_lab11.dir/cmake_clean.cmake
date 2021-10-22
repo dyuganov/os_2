8a0e4c478b1de88a036e41023157482474874daa
mmalakhova <m.malakhova@g.nsu.ru>
2021-10-22 22:23:10 +0700

    adding lab11

---
+++ task11/cmake-build-debug/CMakeFiles/os_lab11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_lab11.dir/main.c.obj"
+  "libos_lab11.dll.a"
+  "os_lab11.exe"
+  "os_lab11.exe.manifest"
+  "os_lab11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/os_lab11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ task11/main.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PUTENV_SUCCESS 0
+
+extern char** environ;
+
+int execvpe(char* file, char* argv[], char* envp[]) {
+    if(file == NULL) {
+        perror("NULL pointer to file");
+        return EXIT_FAILURE;
+    }
+    char** savedEnviron = environ;
+    environ = envp;
+
+    execvp(file, argv);
+	environ = savedEnviron;
+	perror("exec error");
+	return EXIT_FAILURE;
+}
+
+int main(int argc, char* argv[], char* envp[]) {
+
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s [executable] <args>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if(envp == NULL) {
+        perror("Environment is not set");
+    }
+
+    if(putenv("TZ=PST8PDT") != PUTENV_SUCCESS){
+        perror("putenv error");
+	return EXIT_FAILURE;
+    }
+
+    execvpe(argv[1], &argv[1], envp);
+    perror("exec error");
+
+    return EXIT_FAILURE;
+}
