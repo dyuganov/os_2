3199f366ae333c8cb8215e4425743bf3dbd1ab70
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-10-29 23:32:12 +0700

    Update main.c

--- task9/main.c
+++ task9/main.c
@@ -3,26 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-#define _USE_MATH_DEFINES
-#include <math.h>
 #include <signal.h>
+#include <math.h>
 
-#define NUM_STEPS 200000000
+#define NUM_STEPS 2000
 #define SUCCESS 0
 #define ERROR -1
 #define BUFFER_SIZE 256
 
-int threadsNumber;
-pthread_barrier_t barrier;
-pthread_mutex_t mutex;
+long long threadsNumber;
 int stop = 0;
-const long smallIter = 100000;
-int latestIter = 0;
+pthread_mutex_t _mutex = PTHREAD_MUTEX_INITIALIZER;
+long long maxIter = 0;
 
 struct threadInfo {
-    pthread_t threadId;
-    int thread;
+    long long thread;
     double result;
 };
 
@@ -39,98 +34,127 @@ int checkInput(int argc, char** argv) {
     return SUCCESS;
 }
 
-void* calculate(void *threads) {
-    double local_pi = 0.0;
-    struct threadInfo localThreadInfo = *((struct threadInfo*)threads);
-    int bigIter = 0;
-    while(true) {
-        for(long i = localThreadInfo.thread + bigIter * smallIter * threadsNumber; i < localThreadInfo.thread + (bigIter + 1) * smallIter * threadsNumber; i += threadsNumber) {
-            localThreadInfo.result += 1.0/(i*4.0 + 1.0);
-            localThreadInfo.result -= 1.0/(i*4.0 + 3.0);
-        }
-        pthread_barrier_wait(&barrier);
-        pthread_mutex_lock(&mutex);
-        if ((stop == 1) && (bigIter == latestIter)) {
-            pthread_mutex_unlock(&mutex);
-            break;
-        } else {
-            bigIter++;
-            if (latestIter < bigIter) {
-                latestIter = bigIter;
-            }
-            pthread_mutex_unlock(&mutex);
-        }
-
+void sighandler(int signal) {
+    if (signal == SIGINT) {
+        stop = 1;
     }
-
-    ((struct threadInfo *)threads)->result = localThreadInfo.result;
-    pthread_exit(threads);
 }
 
-bool isThreadError(int errorCode, char *argv[]) {
+bool isThreadError(int errorCode) {
     if (errorCode != SUCCESS) {
         char message[BUFFER_SIZE];
         strerror_r(errorCode, message, sizeof message);
-        fprintf(stderr, "%s: Error: %s\n", argv[0], message);
+        fprintf(stderr, "Error: %s\n", message);
         return true;
     }
     return false;
 }
 
-void sighandler(int signal) {
-    stop = 1;
+int lock(pthread_mutex_t *mutex) {
+    int errorCode = pthread_mutex_lock(mutex);
+    if (isThreadError(errorCode)) {
+        pthread_exit((void *) ERROR);
+    }
+    return errorCode;
+}
+
+int unlock(pthread_mutex_t *mutex) {
+    int errorCode = pthread_mutex_unlock(mutex);
+    if (isThreadError(errorCode)) {
+        pthread_exit((void *) ERROR);
+    }
+    return errorCode;
+}
+
+void* calculate(void *threads) {
+    struct threadInfo *localStruct = (struct threadInfo*) threads;
+    if (localStruct == NULL) {
+        fprintf(stderr, "Error with local structure in calculate func\n");
+        pthread_exit(localStruct);
+    }
+    long long thread = localStruct->thread;
+    double local_pi = 0.0;
+    long long iter = 0;
+    while (1) {
+        if (stop == 1 && iter != 0) {
+            lock(&_mutex);
+            if (iter >= maxIter) {
+                unlock(&_mutex);
+                break;
+            }
+            unlock(&_mutex);
+        } else {
+            lock(&_mutex);
+            if (iter > maxIter) {
+                maxIter = iter;
+            }
+            unlock(&_mutex);
+            sched_yield();
+        }
+        long long start = (iter * threadsNumber + thread) * NUM_STEPS;
+        for (long long i = start; i < start + NUM_STEPS; ++i) {
+            local_pi += 1.0 / (i * 4.0 + 1.0);
+            local_pi -= 1.0 / (i * 4.0 + 3.0);
+        }
+        iter++;
+    }
+    localStruct->result = local_pi;
+    pthread_exit(threads);
+}
+
+void delete(pthread_t *threadId,struct threadInfo *threadInfo) {
+    free(threadInfo);
+    free(threadId);
 }
 
 int main(int argc, char** argv) {
     if (checkInput(argc, argv) == ERROR) {
         pthread_exit((void *) ERROR);
     }
+    if (signal(SIGINT, sighandler) == SIG_ERR) {
+        pthread_exit((void *) ERROR);
+    }
     double pi = 0.0;
-
-    int errorCode = pthread_barrier_init(&barrier, NULL, threadsNumber);
-    if (isThreadError(errorCode, argv)) {
-        pthread_exit((void *) ERROR);
-    }
-    errorCode = pthread_mutex_init(&mutex, NULL);
-    if (isThreadError(errorCode, argv)) {
-        pthread_barrier_destroy(&barrier);
-        pthread_exit((void *) ERROR);
-    }
-
-    signal(SIGINT, sighandler);
-
-    struct threadInfo *threads = (struct threadInfo*)malloc(threadsNumber * sizeof(struct threadInfo));
-    if (threads == NULL) {
+    pthread_t *threadId = (pthread_t*) malloc(threadsNumber * sizeof(pthread_t));
+    struct threadInfo *threadInfo = (struct threadInfo*)malloc(threadsNumber * sizeof(struct threadInfo));
+    if (threadInfo == NULL || threadId == NULL) {
         fprintf(stderr, "error with malloc()\n");
-        pthread_barrier_destroy(&barrier);
-        pthread_mutex_destroy(&mutex);
         return EXIT_FAILURE;
     }
 
-    for (int i = 0; i < threadsNumber; ++i) {
-        errorCode = pthread_create(&(threads[i].threadId), NULL, calculate, (void *)(threads + i));
-        if (isThreadError(errorCode, argv)) {
-            free(threads);
-            pthread_barrier_destroy(&barrier);
-            pthread_mutex_destroy(&mutex);
+    long long errorCode, count = 0;
+    for (long long i = 0; i < threadsNumber && stop == 0; ++i) {
+        threadInfo[i].thread = i;
+        errorCode = pthread_create(&threadId[i], NULL, calculate, &threadInfo[i]);
+        if (isThreadError(errorCode)) {
+            delete(threadId, threadInfo);
             pthread_exit((void *) ERROR);
         }
-        threads[i].thread = i;
+        count++;
+    }
+    if (count == 0) {
+        pthread_exit((void *) ERROR);
+    }
+    if (count < threadsNumber) {
+        stop = 1;
     }
 
-    for (int i = 0; i < threadsNumber; ++i) {
-        errorCode = pthread_join(threads[i].threadId, NULL);
-        if (isThreadError(errorCode, argv)) {
-            free(threads);
-            pthread_barrier_destroy(&barrier);
-            pthread_mutex_destroy(&mutex);
+    for (long int i = 0; i < threadsNumber; ++i) {
+        struct threadInfo *local = NULL;
+        errorCode = pthread_join(threadId[i], (void **) &local);
+        if (isThreadError(errorCode)) {
+            delete(threadId, threadInfo);
             pthread_exit((void *) ERROR);
         }
-        pi += threads[i].result;
+        if (local == NULL) {
+            fprintf(stderr, "Error with local structure\n");
+            continue;
+        }
+        pi += local[i].result;
     }
     pi = pi * 4.0;
     printf("Pi = %.15f\n", pi);
     printf("Pi = %.15f\n", M_PI);
-    free(threads);
+    delete(threadId, threadInfo);
     pthread_exit(SUCCESS);
 }
