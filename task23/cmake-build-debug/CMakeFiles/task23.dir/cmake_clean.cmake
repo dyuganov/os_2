75ae48589ff62801faf4fb5d71d8f5e5680e909a
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-11-27 09:17:27 +0700

    add task23

---
+++ task23/cmake-build-debug/CMakeFiles/task23.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task23.dir/main.c.obj"
+  "libtask23.dll.a"
+  "task23.exe"
+  "task23.exe.manifest"
+  "task23.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
